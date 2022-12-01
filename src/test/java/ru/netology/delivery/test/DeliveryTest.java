a68db576f87b51fc8b14e10da9305000fbf7f7f1
Julia <julia.panfilova@mail.ru>
2022-12-01 23:02:22 +0300

    Test modified

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -17,7 +17,7 @@ class DeliveryTest {
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
-        //Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
@@ -30,7 +30,7 @@ class DeliveryTest {
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
         $("[data-test-id='city'] input").setValue(validUser.getCity());
         $("[data-test-id=\"date\"]").click();
-        $x("//input[@placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);//(Keys.chord(Keys.CONTROL + "a"), Keys.BACK_SPACE);
+        $x("//input[@placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
         $x("//input[@placeholder='Дата встречи']").setValue(firstMeetingDate);
         $x("//input[@name='name']").setValue(validUser.getName());
         $("[data-test-id=\"phone\"] .input__control").setValue(validUser.getPhone());
@@ -39,7 +39,7 @@ class DeliveryTest {
         $x("//span[@class='button__text']").click();
         $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
-        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL + "a"), Keys.BACK_SPACE);
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.DELETE);
         $x("//input[@placeholder='Дата встречи']").setValue(secondMeetingDate);
         $x("//span[@class='button__text']").click();
         $x("//*[contains(text(),'Успешно!')]").shouldBe(Condition.visible, Duration.ofSeconds(15));
@@ -47,12 +47,5 @@ class DeliveryTest {
         $x("//span[text()='Перепланировать']").click();
         $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
-
-
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
     }
 }
